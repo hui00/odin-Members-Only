ed7aa5eba9ff08e11e27e35cb752c2aebeeba89b
hui00 <kevinbrinkschulte@hotmail.de>
2022-11-17 17:58:06 +0100

    devise first config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'static_pages/landing_page'
+  get 'static_pages/dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: 'static_pages#landing_page'
   # Defines the root path route ("/")
   # root "articles#index"
 end
