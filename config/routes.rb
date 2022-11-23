c9df493023546a2c5daaac03ebcba0e8cf62d09c
hui00 <kevinbrinkschulte@hotmail.de>
2022-11-23 03:32:00 +0100

    production tailwind 4.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "static_pages/dashboard"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root to: "static_pages#landing_page"
+  root "static_pages#landing_page"
   # Defines the root path route ("/")
   # root "articles#index"
 end
